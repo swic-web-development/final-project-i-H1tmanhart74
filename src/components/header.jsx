a44b89b919b0e86c5e1f7791ac1b88cf112af82a
John Hart <john.hart003@student.swic.edu>
2025-05-15 21:54:54 -0500

    add files and built header

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 className="bg-gradient-to-r from-green-300 to-blue-500 bg-clip-text text-2xl font-bold text-transparent uppercase">
+        SWAPI
+      </h1>
+    </header>
+  )
+}
