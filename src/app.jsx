234120ffc7b7e153dbc3acc3760165d443f89de4
John Hart <john.hart003@student.swic.edu>
2025-05-15 20:40:59 -0500

    add simple SWAPI fetch

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,52 @@
+import { useEffect, useState } from 'react'
+
 export default function App() {
+  const [data] = useState([])
+
+  useEffect(() => {
+    fetch('https://www.swapi.tech/api/vehicles/4')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err))
+  }, [])
+
   return (
     <main>
       <h1 className="text-3xl text-red-500">Hello, World!</h1>
       <p>This is a simple React application.</p>
+      {data && (
+        <div>
+          <h2>{data.name}</h2>
+          <p>Height: {data.model}</p>
+          <p>Mass: {data.mass}</p>
+        </div>
+      )}
     </main>
   )
 }
+
+/*
+
+export default function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/people/1/")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  return (
+    <main>
+      <h1 className="text-3xl text-red-500">Hello, World!</h1>
+      <p>This is a simple React application.</p>
+      {data && (
+        <div>
+          <h2>{data.name}</h2>
+          <p>Height: {data.height}</p>
+          <p>Mass: {data.mass}</p>
+        </div>
+      )}
+    </main>
+  );
+} */
