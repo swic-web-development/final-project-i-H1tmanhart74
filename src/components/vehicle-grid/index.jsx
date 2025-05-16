de23207ce6940a005a1e3a43e8ae777f6850ea74
John Hart <john.hart003@student.swic.edu>
2025-05-15 22:33:01 -0500

    fix index

--- src/components/vehicle-grid/index.jsx
+++ src/components/vehicle-grid/index.jsx
@@ -3,7 +3,7 @@ import Section from './section'
 export default function VehicleGrid({ vehicles }) {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      {vehicles.map((vehicle) => Section({ vehicle })).join('')}
+      {vehicles.map((vehicle) => Section({ vehicle }))}
     </div>
   )
 }
