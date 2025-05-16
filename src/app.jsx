dc476186bfcbf6daadfc15218006ae4e730061ab
John Hart <john.hart003@student.swic.edu>
2025-05-15 22:57:11 -0500

    add fetch to SWAPI

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,27 @@
 import Header from './components/header'
 import VehicleGrid from './components/vehicle-grid'
+import { useState, useEffect } from 'react'
+
 export default function App() {
+  const [vehicles, setVehicles] = useState([])
+
+  useEffect(() => {
+    function fetchVehicles() {
+      fetch('https://swapi.dev/api/vehicles/')
+        .then((response) => response.json())
+        .then((data) => setVehicles(data.results))
+        .catch((error) => {
+          console.error('Error fetching vehicles:', error)
+        })
+    }
+
+    fetchVehicles()
+  }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
       <Header />
-      <VehicleGrid vehicles={[]} />
+      <VehicleGrid vehicles={vehicles} />
     </main>
   )
 }
--- src/components/vehicle-grid/index.jsx
+++ src/components/vehicle-grid/index.jsx
@@ -3,7 +3,9 @@ import Section from './section'
 export default function VehicleGrid({ vehicles }) {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      {vehicles.map((vehicle) => Section({ vehicle }))}
+      {vehicles.map((vehicle) => (
+        <Section vehicle={vehicle} key={vehicle.name} />
+      ))}
     </div>
   )
 }
